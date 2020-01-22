8362143c82461e6e3d423430491145df370bf1ee
yerros <yerisrifan@icloud.com>
2020-01-23 01:46:00 +0700

    03

--- 03.js
+++ 03.js
@@ -1,19 +1,55 @@
 const fs = require('fs')
 class Log {
-    static run(){
-        const date = new Date()
-        const logs = `
-        [${date.toJSON()}] INFO: This is an information about something
-        [${date.toJSON()}] ERROR: We can't divide any numbers by zero
-        [${date.toJSON()}] NOTICE: Someone loves your status.
-        [${date.toJSON()}] WARNING: Insufficient funds.
-        [${date.toJSON()}] DEBUG: This is debug message.
-        [${date.toJSON()}] ALERT: Achtung! Achtung!
-        [${date.toJSON()}] CRITICAL: Medic!! We've got critical damages.
-        [${date.toJSON()}] EMERGENCY: System hung. Contact system administrator immediately!`
-        return fs.writeFileSync('app.log', JSON.stringify(logs) )
+    constructor(){
+        this.date = new Date()
     }
+    info(){
+        return this.save(`[${this.date.toJSON()}] INFO: This is an information about something`)
+    }
+    
+    error() {
+        return this.save(`[${this.date.toJSON()}] ERROR: We can't divide any numbers by zero`)
+    }
+    
+    notice() {
+        return this.save(`[${this.date.toJSON()}] NOTICE: Someone loves your status.`)
+    }
+    
+    warning() {
+        return this.save(`[${this.date.toJSON()}] NOTICE: Someone loves your status.`)
+    }
+    
+    debug() {
+        return this.save(`[${this.date.toJSON()}] DEBUG: This is debug message.`)
+    }
+    critical() {
+        return this.save(`[${this.date.toJSON()}] CRITICAL: Medic!! We've got critical damages.`)
+    }
+    emergency() {
+        return this.save(`[${this.date.toJSON()}] EMERGENCY: System hung. Contact system administrator immediately!`)
+    }
+
+    save(param){
+        fs.appendFile('app.log', param+'\n', (err) => {
+            if(err) throw err;
+            console.log('saved');
+        })
+    }
+
+    find(by,at){
+        console.log(by);
+        const data = fs.readFileSync('app.log', 'utf-8').split('\n')
+        let arr = data.filter(a => a.includes(by) && a.includes(at))
+        //arr.filter(a => a.includes(at))
+        console.log(arr);
+    }
+    
 }
 
+const log = new Log()
 
-console.log(Log.run());
+// log.info();
+// log.emergency();
+// log.error();
+// log.warning();
+log.find('EMERGENCY', '2022-01')

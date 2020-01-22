6f2a4afad9de1b8bb282d31217764cbe3a908720
yerros <yerisrifan@icloud.com>
2020-01-23 00:25:07 +0700

    fix no 6

--- 04.js
+++ 04.js
@@ -1,6 +1,7 @@
-var Datastore = require('nedb')
+const Datastore = require('nedb')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
+const mysql = require('mysql')
 
 class Config {
     constructor(otherClass){
@@ -46,42 +47,86 @@ class ConfigFileStorage {
 
 class ConfigNedb {
     constructor(path){
-        this.db = new Datastore({path, autoload: true})
+        this.db = new Datastore({filename: path, autoload: true})
     }
 
     insert(prop, param){
         let val = {}
         val[prop] = param
         this.db.insert(val, function(err, res){
-           if(res){
-               console.log(res);
-           } else {
-               console.log(err);
-           }
-        })
-    }
-
-    get(val){
-        this.db.find({}, function(err, res) {
             if(res){
                 console.log(res);
             } else {
                 console.log(err);
             }
         })
-        //console.log(val);
+    }
+
+    get(val){
+        const value = val
+        let datareturn = ''
+        this.db.find({}, function(err, res) {
+            if(res){
+                const cari = res.find(a => a[value])
+                datareturn = cari
+                console.log(cari);
+            } else {
+                console.log(err);
+            }
+        })
+        return datareturn
+    }
+
+    unset(val){
+        this.db.find({}, (err,res) => {
+            if(res){
+                const data =  res.find(a => a[val])
+                this.db.remove(data, {multi: true}, (err) => {
+                    if(err) return err
+                })
+            }
+        })
     }
 }
 
+class ConfigMysql {
+    constructor(auth){
+        const connection = mysql.createConnection(auth)
+        connection.connect(function(err) {
+            if(err) throw err;
+            console.log('Database Conneted...');
+        })
+        this.connection = connection;
+    }
+
+    connect(auth){
+        
+    }
+
+    insert(prop, param){
+        let val = {}
+        val[prop] = param;
+        
+        this.connection.query('INSERT INTO nodejs SET ?', toString(val), (err, res, fields) => {
+            if(err) throw err;
+            console.log(res);
+        })
+    }
+}
 //const config = new Config(new ConfigFileStorage('config.json'))
-const config = new Config(new ConfigNedb('config.db'))
+// const config = new Config(new ConfigNedb('config.db'))
+
+const connection = { host: 'localhost' ,user: 'root', password: 'root', database: 'config'}
+const config = new Config(new ConfigMysql(connection))   
+
 
 config.put('site_name', 'Blog')         // Be able to save string.
-config.put('maintenance', false)        // Be able to save boolean.
-config.put('age', 30)                   // Be able to save number.
-config.put('meta', {"description": "lorem ipsum"})
+// config.put('maintenance', false)        // Be able to save boolean.
+// config.put('age', 30)                   // Be able to save number.
+// config.put('meta', {"description": "lorem ipsum"})
 
- config.get('site_name');
+//  config.get('site_name');
 // config.put('site_name', 'Perfect Blog') 
 // config.remove('site_name') 
 // config.get('site_name')  
+
--- 05.js
+++ 05.js
@@ -1,9 +1,55 @@
-const Http = require('http');
-const Https = require('https')
+const http = require('http');
+//const Https = require('https')
 
-/*Https.get('https://httpbin.org/get', res => {
-    console.log(res.headers);
-}) */
+class Http {
+    static get(url){
+        url = url.replace(/^https:\/\//i, 'http://');
+        return new Promise((resolve, reject) => {
+            http.get(url, (res, err) => {
+                if(err) reject(err)
+                console.log(res);
+                resolve(res)
+            })
+        })
+    }
+
+    static post(url, dataPost){
+        const postheaders = {
+            'Content-Type' : 'application/json',
+            'Content-Length' : Buffer.byteLength(JSON.stringify(dataPost), 'utf8')
+        }
+        const option = {
+            host: 'httpbin.org',
+            port: '80',
+            path: '/post',
+            method: 'POST',
+            headers: postheaders
+        }
+        
+        const promise = new Promise((resolve, reject) => {
+            const req = http.request(option, (res) => {
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => {
+                    resolve(`BODY: ${chunk}`);
+                });
+            })
+            
+            req.on('error', (e) => {
+                reject(`problem with request: ${e.message}`);
+            });
+            
+            req.write(JSON.stringify(dataPost));
+            req.end();
+        })
+        return promise;
+    }
+}
+
+// Http.get('https://httpbin.org/get').then(result => {
+//     console.log(result.headers);
+// }).catch((err) => {
+//     console.log(err);
+// })
 
 const data = {
     author_id: 43,
@@ -11,13 +57,8 @@ const data = {
     body: '<p><strong>Lorem ipsum</strong> dolor sit amet</p>',
     created_at: (new Date()).toISOString(),
     tags: ['hello', 'world'],
-  }
-  
-Https.request({
-    host: 'httpbin.org',
-    port: 433,
-    path: '/post',
-    method: 'Post'    
-}, res => {
-    console.log(res);
+}
+
+Http.post('https://httpbin.org/post', data).then(response => {
+  console.log(response);
 })

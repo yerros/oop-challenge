2a504e7823ac3db65493598d743aa888b8f583c1
Yeris <bahtiaryeris@gmail.com>
2020-01-24 15:37:08 +0700

    fix 07

--- 07.js
+++ 07.js
@@ -2,8 +2,8 @@ const data = {
   username: "mul14344",
   email: "email@email.com",
   name: "Mulia",
-  zip: 7000,
-  is_admin: true,
+  zip: 7144,
+  is_admin: false,
   age: 90
 };
 
@@ -26,78 +26,109 @@ class Validator {
     this.data = data;
     this.rules = rules;
     this.message = message;
+    this.passesmessage = [];
+    this.errors = [];
+    this.fail = [];
+    this.autoRun();
   }
 
+  autoRun() {
+    const formessage = this.getProp(this.data);
+    this.runCheck(data.username, rules.username, formessage[0]);
+    this.runCheck(data.email, rules.email, formessage[1]);
+    this.runCheck(data.name, rules.name, formessage[2]);
+    this.runCheck(data.zip, rules.zip, formessage[3]);
+    this.runCheck(data.is_admin, rules.is_admin, formessage[4]);
+    this.runCheck(data.age, rules.age, formessage[5]);
+  }
+  getProp(value) {
+    const prop = Object.keys(value);
+    return prop;
+  }
   passes() {
-    this.runCheck(data.username, rules.username);
-    this.runCheck(data.email, rules.email);
-    this.runCheck(data.name, rules.name);
-    this.runCheck(data.zip, rules.zip);
-    this.runCheck(data.is_admin, rules.is_admin);
-    this.runCheck(data.age, rules.age);
+    console.log(this.passesmessage);
   }
 
-  runCheck = (value, rule) => {
+  fails() {
+    console.log(this.fail);
+  }
+  error() {
+    console.log(this.errors);
+  }
+
+  runCheck = (value, rule, type) => {
     const rules = rule.split("|");
     if (rules.length >= 2) {
       rules.map(item => {
-        const result = this.check(value, item);
-        console.log(result);
+        const result = this.check(value, item, type);
+        return result;
       });
     } else {
-      const result = this.check(value, rule);
-      console.log(result);
+      const result = this.check(value, rule, type);
+      return result;
     }
   };
 
-  check = (value, rule) => {
+  check = (value, rule, type) => {
     if (rule == "required") {
       if (!value) {
-        return this.messages();
+        this.fail.push({ [type]: true });
+        this.errors.push(this.messages(type));
+        return this.messages(type);
       } else {
-        return true;
+        this.passesmessage.push({ [type]: true });
       }
     } else if (rule == "alphanum") {
       const regex = /^[a-z0-9]+$/i;
       const result = value.match(regex);
       if (result == null) {
-        return this.messages();
+        this.fail.push({ [type]: true });
+        this.errors.push(this.messages(type));
+        return this.messages(type);
       } else {
-        return true;
+        this.passesmessage.push({ [type]: true });
       }
     } else if (rule == "email") {
       const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       const result = value.match(regex);
       if (result == null) {
-        return this.messages();
+        this.fail.push({ [type]: true });
+        this.errors.push(this.messages(type));
+        return this.messages(type);
       } else {
-        return true;
+        this.passesmessage.push({ [type]: true });
       }
     } else if (rule == "numeric") {
       if (typeof value == "number") {
-        return true;
+        this.passesmessage.push({ [type]: true });
       } else {
-        return this.messages();
+        this.fail.push({ [type]: true });
+        this.errors.push(this.messages(type));
+        return this.messages(type);
       }
     } else if (rule == "boolean") {
       if (typeof value == "boolean") {
-        return true;
+        this.passesmessage.push({ [type]: true });
       } else {
-        return this.messages();
+        this.fail.push({ [type]: true });
+        this.errors.push(this.messages(type));
+        return this.messages(type);
       }
     } else if (rule == "min:21") {
       if (value <= 21) {
-        return this.messages();
+        this.fail.push({ [type]: true });
+        this.errors.push(this.messages(type));
+        return this.messages(type);
       } else {
-        return true;
+        this.passesmessage.push({ [type]: true });
       }
     } else {
       return "belum di isi";
     }
   };
 
-  messages = () => {
-    return `The %s field is required.`;
+  messages = type => {
+    return `The ${type} field is required.`;
   };
 }
 

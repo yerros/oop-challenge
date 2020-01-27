caf519e7ffe2bbad9ac11ea82e21a867317e4745
Yeris <bahtiaryeris@gmail.com>
2020-01-27 12:56:03 +0700

    fix 06

--- 06.js
+++ 06.js
@@ -1,69 +1,74 @@
 class Auth {
-    constructor(){
-        this.currentUser = [],
-        this.userData = [
-            { id : 1, username: 'root', password: 'secret'},
-            { id : 2, username: 'yeris', password: 'ganteng'}
-        ]
-    }
-    static login(obj){
-        for(let i=0; i < userData.length; i++){
-            if(userData[i].username == obj.username && userData[i].password == obj.password){
-                return currentUser.push(userData[i])
-            } 
+  constructor() {
+    this.currentUser = currentUser;
+    currentUser = this.currentUser;
+  }
+  static login(obj) {
+    if (currentUser.length >= 1) {
+      console.log("You must logut before login again!");
+    } else {
+      for (let i = 0; i < userData.length; i++) {
+        if (
+          userData[i].username == obj.username &&
+          userData[i].password == obj.password
+        ) {
+          console.log("User succefuly login!");
+          return currentUser.push(userData[i]);
         }
-        return currentUser;
+      }
+      return currentUser;
     }
+  }
 
-    static validate(obj){
-        // let validated = ''
-        // for(let i=0; i < userData.length; i++){
-        //     if(userData[i].username == obj.username && userData[i].password == obj.password){ 
-        //         validated = 'Username & Password Match'
-        //         console.log(validated)
-        //     } else {
-        //         validated = 'Username & Password not Match'
-        //         console.log(validated)
-        //     }
-        // }
-        //console.log(validated); 
+  static validate(obj) {
+    // let validated = ''
+    // for(let i=0; i < userData.length; i++){
+    //     if(userData[i].username == obj.username && userData[i].password == obj.password){
+    //         validated = 'Username & Password Match'
+    //         console.log(validated)
+    //     } else {
+    //         validated = 'Username & Password not Match'
+    //         console.log(validated)
+    //     }
+    // }
+    //console.log(validated);
 
-        let validated = userData.find(user => {
-           return user.username == obj.username && user.password == obj.password
-        })
+    let validated = userData.find(user => {
+      return user.username == obj.username && user.password == obj.password;
+    });
 
-        if(validated){
-            console.log(true);
-        } else {
-            console.log(false);
-        }
+    if (validated) {
+      console.log(true);
+    } else {
+      console.log(false);
     }
+  }
 
-    static logout(){
-        if(currentUser){
-            currentUser = null
-            console.log('User succefully Logout')
-        } else {
-            console.log('Are you Logged in?')
-        }
+  static logout() {
+    if (currentUser.length == 0) {
+      console.log("Are you Lodded in?");
+    } else {
+      currentUser = [];
+      console.log("User succefully Logout");
     }
+  }
 
-    static user(){
-        console.log(currentUser)
-    } 
-
+  static user() {
+    console.log(currentUser);
+  }
 }
 
 const userData = [
-    { id : 1, username: 'root', password: 'secret'},
-    { id : 2, username: 'yeris', password: 'ganteng'}
-]
+  { id: 1, username: "root", password: "secret" },
+  { id: 2, username: "yeris", password: "ganteng" }
+];
+let currentUser = [];
 
+// Auth.login({ username: "root", password: "secret" });
+// console.log(currentUser);
+//Auth.login({ username: "root", password: "secret" });
+Auth.validate({ username: "root", password: "secret" });
+//Auth.logout();
 
-
-Auth.login({username: 'yeris', password: 'ganteng'}) 
-//Auth.validate({username: 'root', password: 'secret1'}) 
-//Auth.logout()
-Auth.user()
-
-//console.log(currentUser);
+// Auth.logout();
+// console.log(currentUser);
